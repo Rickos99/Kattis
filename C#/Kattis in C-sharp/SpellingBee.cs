d3fe4e202c7071c82a5e89a57503367414640e57
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2022-10-01 15:26:26 +0200

    Added `Spelling Bee` problem

---
+++ C#/Kattis in C-sharp/SpellingBee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis_in_C_sharp
+{
+    // https://open.kattis.com/problems/spellingbee
+    class SpellingBee
+    {
+        //public static void Main() => Solution();
+
+        static void Solution()
+        {
+            var puzzle = Console.ReadLine();
+            var cases = int.Parse(Console.ReadLine());
+            for (; cases > 0; cases--)
+            {
+                var word = Console.ReadLine();
+                if (IsValidWord(word, puzzle))
+                    Console.WriteLine(word);
+            }
+        }
+
+        private static bool IsValidWord(string word, string puzzle)
+        {
+            if (word.Length < 4) return false;
+            if (word.IndexOf(puzzle[0]) == -1) return false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (puzzle.Contains(word[i]) == false) return false;
+            }
+            return true;
+        }
+    }
+}
