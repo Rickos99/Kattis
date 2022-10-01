748c3c0e77d797f73d5e6b85faa90787642286d6
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2022-10-01 15:24:22 +0200

    Added `Babelfish` problem

---
+++ C#/Kattis in C-sharp/Babelfish.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kattis_in_C_sharp
+{
+    // https://open.kattis.com/problems/babelfish
+    class Babelfish
+    {
+        //public static void Main() => Solution();
+
+        static void Solution()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            string entry;
+            while ((entry = Console.ReadLine()) != string.Empty)
+            {
+                var inputSplit = entry.Split(new[] { ' ' }, 2);
+                dictionary.Add(inputSplit[1], inputSplit[0]);
+            }
+
+            string word;
+            while ((word = Console.ReadLine()) != null)
+            {
+                if (dictionary.TryGetValue(word, out string translation))
+                    Console.WriteLine(translation);
+                else
+                    Console.WriteLine("eh");
+            }
+        }
+    }
+}
