66438a783e3b6c0ec56030b61aff535792ce8e4a
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2023-02-19 16:07:27 +0100

    Added `Pyramids` problem

---
+++ C#/Kattis in C-sharp/Pyramids.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kattis_in_C_sharp
+{
+    // https://open.kattis.com/problems/pyramids
+    internal sealed class Pyramids
+    {
+        //public static void Main() => Solution();
+
+        static void Solution()
+        {
+            var blocks = int.Parse(Console.ReadLine());
+            var height = 0;
+            while (blocks >= 0)
+            {
+                height++;
+                var side = height * 2 - 1;
+                blocks -= side * side;
+            }
+            Console.WriteLine(height - 1);
+        }
+    }
+}
